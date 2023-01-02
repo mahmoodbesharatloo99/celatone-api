6da2045a368e4d5349b3dd4570fa9fc6190f1cc0
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-01-02 17:16:15 +0700

    impv: refactor projects to remove need for duplicate content

--- server/func/projects.py
+++ server/func/projects.py
@@ -1,8 +1,49 @@
 import json
 
 
+def load_project_data(chain, network):
+    accounts = json.load(open(f"registry/data/{chain}/{network}/accounts.json"))
+    assets = json.load(open(f"registry/data/{chain}/{network}/assets.json"))
+    codes = json.load(open(f"registry/data/{chain}/{network}/codes.json"))
+    contracts = json.load(open(f"registry/data/{chain}/{network}/contracts.json"))
+    return accounts, assets, codes, contracts
+
+
+def load_project(entity, accounts, assets, codes, contracts):
+    entity_dict = {}
+    relevant_accounts = [
+        account for account in accounts if account["slug"] == entity["slug"]
+    ]
+    relevant_assets = [asset for asset in assets if entity["slug"] in asset["slugs"]]
+    relevant_codes = [code for code in codes if code["slug"] == entity["slug"]]
+    relevant_contracts = [
+        contract for contract in contracts if contract["slug"] == entity["slug"]
+    ]
+    if len(relevant_codes) == 0 and len(relevant_contracts) == 0:
+        return None
+    entity_dict["slug"] = entity["slug"]
+    entity_dict["details"] = {
+        "name": entity["name"],
+        "website": entity["website"],
+        "github": entity["github"],
+        "logo": entity["logo"],
+        "socials": entity["socials"],
+    }
+    entity_dict["accounts"] = relevant_accounts
+    entity_dict["assets"] = relevant_assets
+    entity_dict["codes"] = relevant_codes
+    entity_dict["contracts"] = relevant_contracts
+    return entity_dict
+
+
 def load_projects(chain, network):
-    projects = json.load(open(f"registry/data/{chain}/{network}/projects.json"))
+    entities = json.load(open(f"registry/data/entities.json"))
+    accounts, assets, codes, contracts = load_project_data(chain, network)
+    projects = []
+    for entity in entities:
+        entity_dict = load_project(entity, accounts, assets, codes, contracts)
+        if entity_dict is not None:
+            projects.append(entity_dict)
     return projects
 
 
@@ -11,7 +52,11 @@ def get_projects(chain, network):
     return projects
 
 
-def get_project(chain, network, project_id):
-    projects = load_projects(chain, network)
-    project = [project for project in projects if project["slug"] == project_id][0]
+def get_project(chain, network, slug):
+    accounts, assets, codes, contracts = load_project_data(chain, network)
+    entities = json.load(open(f"registry/data/entities.json"))
+    entity = [entity for entity in entities if entity["slug"] == slug][0]
+    project = load_project(entity, accounts, assets, codes, contracts)
+    if project is None:
+        return []
     return project
