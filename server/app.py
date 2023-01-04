460bea331bae86126ec991738e36a3283d1249ab
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-01-04 09:38:16 +0700

    feat: add openapi tags

--- server/app.py
+++ server/app.py
@@ -11,12 +11,33 @@ import func.balances as balances
 
 from apiflask import APIFlask
 
-app = APIFlask(__name__)
+app = APIFlask(__name__, title="My API", version="1.0")
+app.config["SYNC_LOCAL_SPEC"] = True
+app.config["LOCAL_SPEC_PATH"] = os.path.join(app.root_path, "openapi.json")
+app.config["TAGS"] = [
+    {
+        "name": "Default",
+        "description": "Default queries",
+    },
+    {
+        "name": "Registry Data",
+        "description": "Queries that uses data from the registry data JSONs",
+    },
+    {
+        "name": "Registry Assets",
+        "description": "Queries that uses data from the registry data asset images",
+    },
+    {
+        "name": "External",
+        "description": "Queries that uses also uses data from external sources",
+    },
+]
 
 # Root
 
 
 @app.route("/", methods=["GET"])
+@app.doc(tags=["Default"])
 def hello_world():
     return {"gm": "gm"}
 
@@ -25,6 +46,7 @@ def hello_world():
 
 
 @app.route("/codes/<chain>/<network>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_codes(chain, network):
     """Get All Codes
 
@@ -34,6 +56,7 @@ def get_codes(chain, network):
 
 
 @app.route("/codes/<chain>/<network>/<code_id>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_code(chain, network, code_id):
     """Get Code by ID
 
@@ -46,6 +69,7 @@ def get_code(chain, network, code_id):
 
 
 @app.route("/contracts/<chain>/<network>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_contracts(chain, network):
     """Get All Contracts
 
@@ -54,6 +78,7 @@ def get_contracts(chain, network):
 
 
 @app.route("/contracts/<chain>/<network>/<contract_address>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_contract(chain, network, contract_address):
     """Get Get Contract by ID
 
@@ -65,6 +90,7 @@ def get_contract(chain, network, contract_address):
 
 
 @app.route("/accounts/<chain>/<network>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_accounts(chain, network):
     """Get All Accounts
 
@@ -74,6 +100,7 @@ def get_accounts(chain, network):
 
 
 @app.route("/accounts/<chain>/<network>/<account_address>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_account(chain, network, account_address):
     """Get Account by ID
 
@@ -86,6 +113,7 @@ def get_account(chain, network, account_address):
 
 
 @app.route("/assets/<chain>/<network>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_assets(chain, network):
     """Get All Assets
 
@@ -95,6 +123,7 @@ def get_assets(chain, network):
 
 
 @app.route("/assets/<chain>/<network>/type/<asset_type>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_asset_by_type(chain, network, asset_type):
     """Get Assets by Type
 
@@ -104,26 +133,31 @@ def get_asset_by_type(chain, network, asset_type):
 
 
 @app.route("/assets/<chain>/<network>/slug/<asset_slug>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_asset_by_slug(chain, network, asset_slug):
     return assets.get_asset_by_slug(chain, network, asset_slug)
 
 
 @app.route("/assets/<chain>/<network>/<asset_id>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_asset(chain, network, asset_id):
     return assets.get_asset(chain, network, asset_id)
 
 
 @app.route("/assets/<chain>/<network>/ibc/<hash>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_asset_ibc(chain, network, hash):
     return assets.get_asset_ibc(chain, network, hash)
 
 
 @app.route("/assets/<chain>/<network>/factory/<creator>/<symbol>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_asset_factory(chain, network, creator, symbol):
     return assets.get_asset_factory(chain, network, creator, symbol)
 
 
 @app.route("/assets/<chain>/<network>/gamm/pool/<pool_id>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_asset_gamm(chain, network, pool_id):
     return assets.get_asset_gamm(chain, network, pool_id)
 
@@ -132,11 +166,13 @@ def get_asset_gamm(chain, network, pool_id):
 
 
 @app.route("/projects/<chain>/<network>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_projects(chain, network):
     return projects.get_projects(chain, network)
 
 
 @app.route("/projects/<chain>/<network>/<project_id>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_project(chain, network, project_id):
     return projects.get_project(chain, network, project_id)
 
@@ -144,12 +180,14 @@ def get_project(chain, network, project_id):
 # Entities
 
 
-@app.route("//entities", methods=["GET"])
+@app.route("/entities", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_entities():
     return entities.get_entities()
 
 
-@app.route("//entities/<entity_slug>", methods=["GET"])
+@app.route("/entities/<entity_slug>", methods=["GET"])
+@app.doc(tags=["Registry Data"])
 def get_entity(entity_slug):
     return entities.get_entity(entity_slug)
 
@@ -158,6 +196,7 @@ def get_entity(entity_slug):
 
 
 @app.route("/balances/<chain>/<network>/<account_address>", methods=["GET"])
+@app.doc(tags=["Registry Data", "External"])
 def get_balances(chain, network, account_address):
     return balances.get_balances(app, chain, network, account_address)
 
@@ -166,6 +205,7 @@ def get_balances(chain, network, account_address):
 
 
 @app.route("/images/entities/<entity_slug>", methods=["GET"])
+@app.doc(tags=["Registry Assets"])
 def get_entity_image(entity_slug):
     return send_file(f"../registry/assets/entities/{entity_slug}.png")
 
