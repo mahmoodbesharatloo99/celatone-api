8be336e02772f2d8ca3fa604a4d8a6618b4eb2b0
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-01-24 15:03:22 +0700

    Revert "feat: add website to chain data"
    This reverts commit 6f75f1eb52b2d6d6afcc1c672fd6f016c8f032ce.

--- server/func/codes.py
+++ server/func/codes.py
@@ -2,7 +2,7 @@ import json
 import requests
 import os
 
-from func.constants import SCANWORKS_URL, GRAPHQL_DICT
+from func.constants import SCANWORKS_URL
 
 
 def load_codes(chain, network):
@@ -19,21 +19,6 @@ def load_codes(chain, network):
                 code["verification_details"] = verification_details[code["id"]]
             else:
                 code["verified"] = False
-        code_id = [code["id"] for code in codes]
-        payload = (
-            """{"query":
-    "query { codes(where:{id:{_in:%s}}) {id access_config_permission contracts_aggregate { aggregate{ count } } } }",
-    "variables":{}}"""
-            % code_id
-        )
-        code_response = requests.request(
-            "POST", GRAPHQL_DICT[chain][network], data=payload
-        ).json()["data"]["codes"]
-        for i in range(len(code_response)):
-            code["permission"] = code_response[i]["access_config_permission"]
-            code["contracts"] = code_response[i]["contracts_aggregate"]["aggregate"][
-                "count"
-            ]
     return codes
 
 
