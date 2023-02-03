232669e5f6d02b9b06804fc4d8a4a1c6de74fe73
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-02-03 16:56:51 +0700

    fix: asset price

--- server/func/price.py
+++ server/func/price.py
@@ -6,8 +6,8 @@ def get_prices(assets):
     slugs = [asset["coingecko"] for asset in assets if asset["coingecko"] != None]
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={','.join(slugs)}&vs_currencies=usd&x_cg_pro_api_key={COINGECKO_API_KEY}"
     price_response = requests.get(url).json()
-    asset["price"] = 0
     for asset in assets:
+        asset["price"] = 0
         if asset["coingecko"] != None:
             asset["price"] = price_response[asset["coingecko"]]["usd"]
     return assets
