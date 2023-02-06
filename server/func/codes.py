e177c6efe333d32484be398830dc7ca51626ad1d
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-02-06 12:25:14 +0700

    feat: add cw2 info to code

--- server/func/codes.py
+++ server/func/codes.py
@@ -1,9 +1,28 @@
 import json
 import requests
 import os
+import base64
 
-from func.constants import SCANWORKS_URL
+from func.constants import SCANWORKS_URL, LCD_DICT
 from func.graphql import get_graphql_code_details
+
+
+def get_cw2_details(chain, network, code_id):
+    cw2_info = ""
+    try:
+        lcd = LCD_DICT[chain][network]
+        contract = requests.get(
+            f"{lcd}/cosmwasm/wasm/v1/code/{code_id}/contracts"
+        ).json()["contracts"][0]
+        cw2_details = requests.get(
+            f"{lcd}/cosmwasm/wasm/v1/contract/{contract}/raw/Y29udHJhY3RfaW5mbw%3D%3D"
+        ).json()
+        cw2_parsed = json.loads(base64.b64decode(cw2_details["data"]).decode("utf-8"))
+        cw2_info = f"{cw2_parsed['contract']} ({cw2_parsed['version']})"
+    except Exception as e:
+        print(e)
+        pass
+    return cw2_info
 
 
 def load_codes(chain, network):
@@ -23,6 +42,7 @@ def load_codes(chain, network):
             ][0]
             code["uploader"] = code_graphql_detail["creator"]
             code["contracts"] = code_graphql_detail["contract_instantiated"]
+            code["cw2"] = get_cw2_details(chain, network, code["id"])
             code["instantiatePermission"] = code_graphql_detail[
                 "access_config_permission"
             ]
