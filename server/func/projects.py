48a61da1f09f0605ac43b1db53559c7220c30663
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-02-20 16:35:52 +0700

    fix: fix public project condition

--- server/func/projects.py
+++ server/func/projects.py
@@ -27,24 +27,20 @@ def load_project(entity, accounts, assets, codes, contracts):
         "socials": entity["socials"],
     }
     # only keep accounts for this entity
-    relevant_accounts = [
-        account for account in accounts if account["slug"] == entity["slug"]
-    ]
+    relevant_accounts = [account for account in accounts if account["slug"] == entity["slug"]]
     # only keep assets for this entity
     relevant_assets = [asset for asset in assets if entity["slug"] in asset["slugs"]]
     # only keep codes for this entity
     relevant_codes = [code for code in codes if code["slug"] == entity["slug"]]
     # only keep contracts for this entity
-    relevant_contracts = [
-        contract for contract in contracts if contract["slug"] == entity["slug"]
-    ]
-    if len(relevant_codes) == 0 and len(relevant_contracts) == 0:
-        return None
-    entity_dict["accounts"] = relevant_accounts
-    entity_dict["assets"] = relevant_assets
-    entity_dict["codes"] = relevant_codes
-    entity_dict["contracts"] = relevant_contracts
-    return entity_dict
+    relevant_contracts = [contract for contract in contracts if contract["slug"] == entity["slug"]]
+    if len(relevant_accounts) != 0 and len(relevant_codes) != 0 and len(relevant_contracts) != 0:
+        entity_dict["accounts"] = relevant_accounts
+        entity_dict["assets"] = relevant_assets
+        entity_dict["codes"] = relevant_codes
+        entity_dict["contracts"] = relevant_contracts
+        return entity_dict
+    return None
 
 
 def load_projects(chain, network):
