bb54afde4550aa90908126b84444f72c72466fde
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-02-22 16:43:57 +0700

    feat: order osmosis-1 contracts

--- scripts/format.py
+++ scripts/format.py
@@ -1,6 +1,6 @@
 import json
 
-entities = json.loads(open("./registry/data/entities.json").read())
-sorted_entities = sorted(entities, key=lambda k: k["name"])
+entities = json.loads(open("./registry/data/osmosis/osmosis-1/contracts.json").read())
+sorted_entities = sorted(entities, key=lambda k: k["slug"])
 
-json.dump(sorted_entities, open("./registry/data/entities.json", "w"), indent=2)
+json.dump(sorted_entities, open("./registry/data/osmosis/osmosis-1/contracts.json", "w"), indent=2)
