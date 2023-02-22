38d0ff74cdb9a95614ecfa34644294abb07fac4d
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-02-22 12:58:04 +0700

    feat: fix handling empty contract list

--- server/func/contracts.py
+++ server/func/contracts.py
@@ -3,22 +3,22 @@ from func.graphql import get_contract_instantiator_admin
 
 
 def get_contracts(chain, network):
+    contracts = []
     contracts = load_and_check_registry_data(chain, network, "contracts")
-    instantiator_admin_data = get_contract_instantiator_admin(
-        chain, network, [contract["address"] for contract in contracts]
-    )
-    for contract in contracts:
-        for data in instantiator_admin_data:
-            if contract["address"] == data["address"]:
-                contract["instantiator"] = data["instantiator"]
-                contract["admin"] = data["admin"]
-                contract["label"] = data["label"]
+    if len(contracts) > 0:
+        instantiator_admin_data = get_contract_instantiator_admin(
+            chain, network, [contract["address"] for contract in contracts]
+        )
+        for contract in contracts:
+            for data in instantiator_admin_data:
+                if contract["address"] == data["address"]:
+                    contract["instantiator"] = data["instantiator"]
+                    contract["admin"] = data["admin"]
+                    contract["label"] = data["label"]
     return contracts
 
 
 def get_contract(chain, network, contract_address):
     contracts = get_contracts(chain, network)
-    contract = [
-        contract for contract in contracts if contract["address"] == contract_address
-    ][0]
+    contract = [contract for contract in contracts if contract["address"] == contract_address][0]
     return contract
