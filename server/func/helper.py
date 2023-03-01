f05db6377bfc28985a0ececf93a2f8a14060b8b3
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-03-01 16:46:07 +0700

    feat: initial real price

--- server/func/helper.py
+++ server/func/helper.py
@@ -31,9 +31,7 @@ def get_hive_balance(chain, network, account_address):
     contract_address_chunks = split(contract_addresses, 50)
     for contract_address_chunk in contract_address_chunks:
         query = generate_hive_query(account_address, contract_address_chunk)
-        hive_data = requests.post(
-            f"{constants.HIVE_DICT[network]}/graphql", json={"query": query}
-        ).json()["data"]
+        hive_data = requests.post(f"{constants.HIVE_DICT[network]}/graphql", json={"query": query}).json()["data"]
         for contract_address, data in hive_data.items():
             if data is None or data["contractQuery"] is None:
                 continue
@@ -55,23 +53,18 @@ def get_hive_balance(chain, network, account_address):
 
 def get_native_balances(endpoint, chain, network, account_address):
     output_balance = []
-    balances = requests.get(
-        f"{endpoint}/cosmos/bank/v1beta1/balances/{account_address}?pagination.limit=500"
-    )
+    balances = requests.get(f"{endpoint}/cosmos/bank/v1beta1/balances/{account_address}?pagination.limit=500")
     balances = balances.json()
 
     # Get all supported assets for this chain and network
     supported_assets = assets.get_assets_by_type(chain, network, "native")
+    supported_assets_prices = prices.get_prices([supported_asset["id"] for supported_asset in supported_assets])
 
     if "balances" in balances:
         for balance in balances["balances"]:
             # Check if the balance is a supported asset
             if balance["denom"] in [asset["id"] for asset in supported_assets]:
-                asset = [
-                    asset
-                    for asset in supported_assets
-                    if asset["id"] == balance["denom"]
-                ][0]
+                asset = [asset for asset in supported_assets if asset["id"] == balance["denom"]][0]
                 output_balance.append(
                     {
                         "name": asset["name"],
@@ -80,7 +73,7 @@ def get_native_balances(endpoint, chain, network, account_address):
                         "amount": balance["amount"],
                         "precision": asset["precision"],
                         "type": "native",
-                        "price": 1.00,
+                        "price": supported_assets_prices[asset["id"]],
                     }
                 )
             # If it's not a supported asset, just return the balance with no extra info
--- server/func/prices.py
+++ server/func/prices.py
@@ -1,53 +1,8 @@
 import requests
-from func.constants import COINGECKO_API_KEY
 
 
-""" def get_prices(assets):
-    slugs = [asset["coingecko"] for asset in assets if asset["coingecko"] != None]
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={','.join(slugs)}&vs_currencies=usd&x_cg_pro_api_key={COINGECKO_API_KEY}"
-    price_response = requests.get(url).json()
-    for asset in assets:
-        asset["price"] = 0
-        if asset["coingecko"] != None:
-            slug = asset["coingecko"]
-            price_data = price_response[slug]
-            price_usd = price_data["usd"]
-            asset["price"] = price_usd
-    return assets
-
-
-def get_price(asset):
-    slug = asset["coingecko"]
-    asset["price"] = 0
-    if slug is not None:
-        url = f"https://api.coingecko.com/api/v3/simple/price?ids={slug}&vs_currencies=usd&x_cg_pro_api_key={COINGECKO_API_KEY}"
-        price_response = requests.get(url).json()
-        asset["price"] = price_response[slug]["usd"]
-    return asset """
-
-
-def get_prices(assets):
-    return [1.00] * len(assets)
-
-
-def get_price(asset):
-    return 1.00
-
-
-if __name__ == "__main__":
-    get_prices(
-        [
-            {
-                "name": "Osmosis",
-                "symbol": "OSMO",
-                "logo": "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmo.svg",
-                "description": "The native token of Osmosis",
-                "type": "native",
-                "id": "uosmo",
-                "precision": 6,
-                "slugs": ["osmosis"],
-                "coingecko": "osmosis",
-                "coinmarketcap": "",
-            }
-        ]
-    )
+def get_prices(ids):
+    prices = requests.get(
+        f"https://celatone-price-cacher-h2bc4rnx5a-as.a.run.app/prices?denoms={','.join(ids)}"
+    ).json()
+    return prices
