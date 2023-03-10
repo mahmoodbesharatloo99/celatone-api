b8a37ca377eedd608cc6d90f45643f1f15f66ebd
bubu <bkioshn@gmail.com>
2023-03-10 16:46:47 +0700

    fix: constant env default value

--- server/func/constants.py
+++ server/func/constants.py
@@ -1,10 +1,10 @@
 import ast
 import os
 
-LCD_DICT = ast.literal_eval(os.getenv("LCD_DICT", None))
-HIVE_DICT = ast.literal_eval(os.getenv("HIVE_DICT", None))
-GRAPHQL_DICT = ast.literal_eval(os.getenv("GRAPHQL_DICT", None))
-GRAPHQL_TEST_DICT = ast.literal_eval(os.getenv("GRAPHQL_TEST_DICT", None))
+LCD_DICT = ast.literal_eval(os.getenv("LCD_DICT", ""))
+HIVE_DICT = ast.literal_eval(os.getenv("HIVE_DICT",""))
+GRAPHQL_DICT = ast.literal_eval(os.getenv("GRAPHQL_DICT",""))
+GRAPHQL_TEST_DICT = ast.literal_eval(os.getenv("GRAPHQL_TEST_DICT",""))
 SCANWORKS_URL = os.getenv("SCANWORKS_URL", None)
 PRICE_CACHER_URL = os.getenv("PRICE_CACHER_URL", None)
 COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY", None)
