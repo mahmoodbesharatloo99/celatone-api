209e2eb1edd0d9afc2ae68ed8f7acca4aefce934
bubu <bkioshn@gmail.com>
2023-03-15 11:05:16 +0700

    fix: add try except to get_tx

--- server/func/transaction.py
+++ server/func/transaction.py
@@ -5,10 +5,13 @@ from func.lcd import get_lcd_transaction
 
 def get_tx(chain, network, tx_hash):
     # get data from graphql
-    graphql_res = get_graphql_transaction(chain, network, tx_hash, 1)
-    graphql_tx_res = graphql_res.json()["data"]["lcd_tx_responses"]
-    if len(graphql_tx_res):
-        return graphql_tx_res[0]["result"]
+    try:
+        graphql_res = get_graphql_transaction(chain, network, tx_hash, 1)
+        graphql_tx_res = graphql_res.json()["data"]["lcd_tx_responses"]
+        if len(graphql_tx_res):
+            return graphql_tx_res[0]["result"]
+    except:
+        print("Graphql error")
 
     # if no data from graphql, get data from lcd
     lcd_res = get_lcd_transaction(chain, network, tx_hash)
