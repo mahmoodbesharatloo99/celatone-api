b769373df8c2d0230703e9dbff7349ddf5d293a3
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-06-14 15:08:23 +0700

    fix: add raise for status to transaction query

--- server/func/transaction.py
+++ server/func/transaction.py
@@ -6,12 +6,14 @@ from func.lcd import get_lcd_transaction
 
 def get_tx(chain, network, tx_hash):
     try:
-        lcd = get_lcd_transaction(chain, network, tx_hash).json()
-        return lcd
+        lcd = get_lcd_transaction(chain, network, tx_hash)
+        lcd.raise_for_status()
+        return lcd.json()
     except Exception as e:
         logging.error(f"Error getting lcd transaction: {e}")
     try:
         graphql_res = get_lcd_tx_results(chain, network, tx_hash)
+        graphql_res.raise_for_status()
         graphql_tx_res = graphql_res.json()["data"]["lcd_tx_results"]
         if graphql_tx_res:
             return graphql_tx_res[0]["result"]
@@ -19,6 +21,7 @@ def get_tx(chain, network, tx_hash):
         logging.error(f"Error getting lcd_tx_results: {e}")
     try:
         graphql_res = get_lcd_tx_responses(chain, network, tx_hash, 1)
+        graphql_res.raise_for_status()
         graphql_tx_res = graphql_res.json()["data"]["lcd_tx_responses"]
         if graphql_tx_res:
             return graphql_tx_res[0]["result"]
