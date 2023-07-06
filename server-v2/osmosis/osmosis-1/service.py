09624d3bb3c1d9c349c3a5edd4562c4aa59def2c
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2023-07-06 15:22:08 +0700

    Implement OsmosisBaseService + Osmosis1Serevice For testing

---
+++ server-v2/osmosis/osmosis-1/service.py
@@ -0,0 +1,5 @@
+from osmosis_base_service import OsmosisBaseService
+
+class Osmosis1Service(OsmosisBaseService):
+    chain: str = "osmosis"
+    network: str = "osmosis-1"
---
+++ server-v2/osmosis/osmosis_base_service.py
@@ -0,0 +1,14 @@
+from typing import Any, List, Dict
+from base.base_service import BaseService
+
+class OsmosisBaseService(BaseService):
+    def get_asset_factory(self, creator, symbol):
+        assets = self.get_assets_with_prices()
+        asset_map = {asset["id"]: asset for asset in assets}
+        return asset_map[f"factory/{creator}/{symbol}"]
+
+
+    def get_asset_gamm(self, pool_id):
+        assets = self.get_assets_with_prices()
+        asset_map = {asset["id"]: asset for asset in assets}
+        return asset_map[f"gamm/pool/{pool_id}"]
