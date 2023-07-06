b449d18a6c5f95d8df3a54daddf6a3a31c5a96ac
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2023-07-06 14:00:05 +0700

    Add price.py to utils

---
+++ server-v2/utils/price.py
@@ -0,0 +1,19 @@
+import requests
+from constants import PRICE_CACHER_URL
+
+def chunks(ids):
+    n = 10
+    for i in range(0, len(ids), n):
+        yield ids[i : i + n]
+
+
+def get_prices(chain, network, ids):
+    prices = {}
+    id_chunks = chunks(ids)
+    for id_chunk in id_chunks:
+        prices.update(
+            requests.get(
+                f"{PRICE_CACHER_URL}/{chain}/{network}?ids={','.join(id_chunk)}"
+            ).json()
+        )
+    return prices
