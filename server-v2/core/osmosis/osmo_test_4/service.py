4c1ca13ffc9836ec02fe819440f3245e490e2ca6
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2023-07-18 13:39:57 +0700

    Oops

--- server-v2/core/osmosis/osmo_test_4/service.py
+++ server-v2/core/osmosis/osmo_test_4/service.py
@@ -2,4 +2,4 @@ from ..osmosis_base_service import OsmosisBaseService
 
 class OsmosisTest4Service(OsmosisBaseService):
     chain: str = "osmosis"
-    network: str = "osmo-test-1"
+    network: str = "osmo-test-4"
