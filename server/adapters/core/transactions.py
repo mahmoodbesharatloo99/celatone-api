619dc26eb155034a31444526963d06b5814c8f6e
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-08-24 15:43:15 +0700

    impv: add wld to transaction fallback

--- server/adapters/core/transactions.py
+++ server/adapters/core/transactions.py
@@ -2,11 +2,15 @@ import logging
 import requests
 from flask import abort
 from utils.graphql import get_lcd_tx_responses, get_lcd_tx_results
-from constants import LCD_DICT
+from constants import LCD_DICT, WLD_URL
 
 
 def get_lcd_transaction(chain, network, tx_hash):
     return requests.get(f"{LCD_DICT[chain][network]}/cosmos/tx/v1beta1/txs/{tx_hash}")
+
+
+def get_wld_transaction(tx_hash):
+    return requests.get(f"{WLD_URL}/txs/{tx_hash}")
 
 
 def get_tx(chain, network, tx_hash):
@@ -32,4 +36,11 @@ def get_tx(chain, network, tx_hash):
         return lcd.json()
     except Exception as e:
         logging.error(f"Error getting lcd transaction: {e}")
+    if chain == "sei" and network == "pacific-1":
+        try:
+            wld = get_wld_transaction(tx_hash)
+            wld.raise_for_status()
+            return wld.json()
+        except Exception as e:
+            logging.error(f"Error getting wld transaction: {e}")
     return abort(404)
