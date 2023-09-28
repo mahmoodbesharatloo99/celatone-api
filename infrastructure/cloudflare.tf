dd5cf0391046315e0783d6a5c3cd55e180aa1705
Wasin Watthanasrisong <wasinwatt@gmail.com>
2023-09-28 11:34:37 +0700

    [cloudflare] add acme and full ssl page rule

---
+++ infrastructure/cloudflare.tf
@@ -0,0 +1,30 @@
+resource "cloudflare_record" "celatone_api" {
+  zone_id = local.cloudflare_zone_id
+  name    = "celatone-api-${var.environment}"
+  type    = "CNAME"
+  value   = "ghs.googlehosted.com."
+  ttl     = 1
+  proxied = true
+}
+
+// disable browser integrity check, turns off Automatic HTTPS rewrites
+resource "cloudflare_page_rule" "acme" {
+  zone_id  = local.cloudflare_zone_id
+  target   = "celatone-api-${var.environment}.celatone.com/.well-known/acme-challenge/*"
+  priority = 1
+
+  actions {
+    automatic_https_rewrites = "off"
+    browser_check            = "off"
+  }
+}
+
+resource "cloudflare_page_rule" "full_ssl" {
+  zone_id  = local.cloudflare_zone_id
+  target   = "celatone-api-${var.environment}.celatone.com/*"
+  priority = 2
+
+  actions {
+    ssl = "full"
+  }
+}
--- infrastructure/dns.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_record" "celatone_api" {
-  zone_id = local.cloudflare_zone_id
-  name    = "celatone-api-${var.environment}"
-  type    = "CNAME"
-  value   = "ghs.googlehosted.com."
-  ttl     = 1
-  proxied = true
-}
