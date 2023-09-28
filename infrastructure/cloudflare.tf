6a787b3a3356401c0a35d6d22c061ec1f3dc377f
Wasin Watthanasrisong <wasinwatt@gmail.com>
2023-09-28 13:47:14 +0700

    [cloudflare] test API rate limit

--- infrastructure/cloudflare.tf
+++ infrastructure/cloudflare.tf
@@ -28,3 +28,35 @@ resource "cloudflare_page_rule" "full_ssl" {
     ssl = "full"
   }
 }
+
+resource "cloudflare_ruleset" "rate_limit" {
+  zone_id     = local.cloudflare_zone_id
+  name        = "celatone-api-${var.environment}-rate-limit"
+  description = "Rate limit Celatone API requests to 1000 per minute"
+  kind        = "zone"
+  phase       = "http_ratelimit"
+
+  rules {
+    action = "block"
+    action_parameters {
+      response {
+        status_code  = 429
+        content      = "{\"message\": \"rate limit reached\"}"
+        content_type = "application/json"
+      }
+    }
+
+    ratelimit {
+      characteristics     = ["cf.colo.id", "ip.src"]
+      period              = 60
+      requests_per_period = 3
+      mitigation_timeout  = 60
+      requests_to_origin  = false
+    }
+
+    expression  = "(http.host eq \"celatone-api-${var.environment}.alleslabs.dev\")"
+    description = "Rate limit Celatone API requests to 1000 per minute"
+    enabled     = true
+  }
+}
+
