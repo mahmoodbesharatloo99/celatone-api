2618207cdf5de6a72e097278cc82cf265a7cb4bd
Sawit Trisirisatayawong <sawit@alleslabs.com>
2023-10-09 12:49:55 +0300

    feat: remove registry files (#30)

--- scripts/format.py
+++
@@ -1,6 +0,0 @@
-import json
-
-entities = json.loads(open("./registry/data/osmosis/osmosis-1/contracts.json").read())
-sorted_entities = sorted(entities, key=lambda k: k["name"])
-
-json.dump(sorted_entities, open("./registry/data/osmosis/osmosis-1/contracts.json", "w"), indent=2)
