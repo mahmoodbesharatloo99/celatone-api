d6edc438b49b4505062ff1ca43b671de56558440
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-10-30 12:37:50 +0700

    feat: update move vm module to move_modules

--- server/adapters/aldus/modules.py
+++ server/adapters/aldus/modules.py
@@ -2,10 +2,10 @@ from typing import List, Dict, Any
 from utils.aldus import get_aldus_chain_data
 
 
-class ModuleManager:
+class MoveModuleManager:
     def __init__(self, chain: str, network: str):
         """
-        Initializes the ModuleManager with module data.
+        Initializes the MoveModuleManager with module data.
 
         Parameters:
         chain (str): The blockchain chain.
@@ -13,9 +13,9 @@ class ModuleManager:
         """
         self.chain = chain
         self.network = network
-        self.modules = self.load_modules(chain, network)
+        self.modules = self.load_move_modules(chain, network)
 
-    def load_modules(self, chain: str, network: str) -> List[Dict[str, Any]]:
+    def load_move_modules(self, chain: str, network: str) -> List[Dict[str, Any]]:
         """
         Fetches module data.
 
@@ -25,7 +25,7 @@ class ModuleManager:
         modules = get_aldus_chain_data(chain, network, "modules")
         return modules
 
-    def get_modules(self) -> List[Dict[str, Any]]:
+    def get_move_modules(self) -> List[Dict[str, Any]]:
         """
         Fetches module data.
 
@@ -34,7 +34,7 @@ class ModuleManager:
         """
         return self.modules
 
-    def get_module(self, address: str, name: str) -> Dict[str, Any]:
+    def get_move_module(self, address: str, name: str) -> Dict[str, Any]:
         """
         Fetches a specific module by its address and name.
 
--- server/routes/initia.py
+++ server/routes/initia.py
@@ -1,22 +1,22 @@
 from apiflask import APIBlueprint
-from adapters.aldus.modules import ModuleManager
+from adapters.aldus.modules import MoveModuleManager
 
 initia_bp = APIBlueprint("initia", __name__)
 
 
-@initia_bp.route("/<chain>/<network>/modules", methods=["GET"])
-def get_modules(chain, network):
+@initia_bp.route("/<chain>/<network>/move_modules", methods=["GET"])
+def get_move_modules(chain, network):
     """Get All Codes
 
     Returns a list of all the known codes based on the input chain and network
     """
-    return ModuleManager(chain, network).get_modules()
+    return MoveModuleManager(chain, network).get_move_modules()
 
 
-@initia_bp.route("/<chain>/<network>/modules/<address>/<name>", methods=["GET"])
-def get_module(chain, network, address, name):
+@initia_bp.route("/<chain>/<network>/move_modules/<address>/<name>", methods=["GET"])
+def get_move_module(chain, network, address, name):
     """Get Code by ID
 
     Returns a specific code based on the input chain, network, and code_id
     """
-    return ModuleManager(chain, network).get_module(address, name)
+    return MoveModuleManager(chain, network).get_move_module(address, name)
