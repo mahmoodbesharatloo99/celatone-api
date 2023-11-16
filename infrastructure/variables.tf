152e11df2f6dfcab63c96b31429c4f7ebff7e68b
evilpeach <kanisorn.peach@gmail.com>
2023-11-16 17:19:40 +0700

    update terraform

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,3 +1,7 @@
+variable "aldus_url" {
+  type = string
+}
+
 variable "environment" {
   type = string
 }
@@ -6,27 +10,7 @@ variable "image_url" {
   type = string
 }
 
-variable "lcd_dict" {
-  type = string
-}
-
-variable "hive_dict" {
-  type = string
-}
-
-variable "graphql_dict" {
-  type = string
-}
-
-variable "scanworks_url" {
-  type = string
-}
-
 variable "price_cacher_url" {
-  type = string
-}
-
-variable "graphql_test_dict" {
   type = string
 }
 
