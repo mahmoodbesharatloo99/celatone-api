0db202fd1f1878ac7daded8e996847c4638f7b52
evilpeach <kanisorn.peach@gmail.com>
2023-11-17 15:59:09 +0700

    test: account table count

--- server/tests/test_accounts.py
+++ server/tests/test_accounts.py
@@ -26,3 +26,21 @@ def test_get_account_info_failed():
         f"/v1/{chain}/{network}/accounts/{account_address}/info"
     )
     assert response.status_code == 500
+
+
+def test_get_account_table_count():
+    chain = "osmosis"
+    network = "osmo-test-5"
+    account_address = "osmo1acqpnvg2t4wmqfdv8hq47d3petfksjs5r9t45p"
+
+    response = app.test_client().get(
+        f"/v1/{chain}/{network}/accounts/{account_address}/table-count"
+    )
+    assert response.status_code == 200
+
+    response_json = response.get_json()
+    assert type(response_json["tx"]) == int
+    assert type(response_json["proposal"]) == int
+    assert type(response_json["code"]) == int
+    assert type(response_json["instantiated"]) == int
+    assert type(response_json["contract_by_admin"]) == int
