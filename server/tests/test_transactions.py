d49810d41ce93fc20ce9c43cebd1a0342a9cbb3b
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-11-17 14:33:58 +0700

    fix: tests

--- server/tests/test_transactions.py
+++ server/tests/test_transactions.py
@@ -16,24 +16,24 @@ def test_transactions_wasm():
     assert len(response_json["items"]) == limit
     assert type(response_json["total"]) == int
 
-    item = response_json["items"][0]
-    assert type(item["block"]["height"]) == int
-    assert type(item["block"]["timestamp"]) == str
-    assert type(item["hash"]) == str
-    assert type(item["success"]) == bool
-    assert type(item["messages"]) == list
-    assert type(item["is_send"]) == bool
-    assert type(item["is_ibc"]) == bool
-    assert type(item["is_clear_admin"]) == bool
-    assert type(item["is_execute"]) == bool
-    assert type(item["is_instantiate"]) == bool
-    assert type(item["is_migrate"]) == bool
-    assert type(item["is_store_code"]) == bool
-    assert type(item["is_update_admin"]) == bool
-    assert item.get("is_move_publish") is None
-    assert item.get("is_move_upgrade") is None
-    assert item.get("is_move_execute") is None
-    assert item.get("is_move_script") is None
+    for item in response_json["items"]:
+        assert type(item["block"]["height"]) == int
+        assert type(item["block"]["timestamp"]) == str
+        assert type(item["hash"]) == str
+        assert type(item["success"]) == bool
+        assert type(item["messages"]) == list
+        assert type(item["is_send"]) == bool
+        assert type(item["is_ibc"]) == bool
+        assert type(item["is_clear_admin"]) == bool
+        assert type(item["is_execute"]) == bool
+        assert type(item["is_instantiate"]) == bool
+        assert type(item["is_migrate"]) == bool
+        assert type(item["is_store_code"]) == bool
+        assert type(item["is_update_admin"]) == bool
+        assert item.get("is_move_publish") is None
+        assert item.get("is_move_upgrade") is None
+        assert item.get("is_move_execute") is None
+        assert item.get("is_move_script") is None
 
 
 def test_transactions_wasm():
@@ -51,21 +51,21 @@ def test_transactions_wasm():
     assert len(response_json["items"]) == limit
     assert type(response_json["total"]) == int
 
-    item = response_json["items"][0]
-    assert type(item["block"]["height"]) == int
-    assert type(item["block"]["timestamp"]) == str
-    assert type(item["hash"]) == str
-    assert type(item["success"]) == bool
-    assert type(item["messages"]) == list
-    assert type(item["is_send"]) == bool
-    assert type(item["is_ibc"]) == bool
-    assert item.get("is_clear_admin") is None
-    assert item.get("is_execute") is None
-    assert item.get("is_instantiate") is None
-    assert item.get("is_migrate") is None
-    assert item.get("is_store_code") is None
-    assert item.get("is_update_admin") is None
-    assert type(item["is_move_publish"]) == bool
-    assert type(item["is_move_upgrade"]) == bool
-    assert type(item["is_move_execute"]) == bool
-    assert type(item["is_move_script"]) == bool
+    for item in response_json["items"]:
+        assert type(item["block"]["height"]) == int
+        assert type(item["block"]["timestamp"]) == str
+        assert type(item["hash"]) == str
+        assert type(item["success"]) == bool
+        assert type(item["messages"]) == list
+        assert type(item["is_send"]) == bool
+        assert type(item["is_ibc"]) == bool
+        assert item.get("is_clear_admin") is None
+        assert item.get("is_execute") is None
+        assert item.get("is_instantiate") is None
+        assert item.get("is_migrate") is None
+        assert item.get("is_store_code") is None
+        assert item.get("is_update_admin") is None
+        assert type(item["is_move_publish"]) == bool
+        assert type(item["is_move_upgrade"]) == bool
+        assert type(item["is_move_execute"]) == bool
+        assert type(item["is_move_script"]) == bool
