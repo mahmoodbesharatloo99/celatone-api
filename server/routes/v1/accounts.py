34c8d8d4527dd73e9b1f3a42e2309fcef5f75b9f
evilpeach <kanisorn.peach@gmail.com>
2023-11-18 15:00:46 +0700

    feat: account proposal

--- server/routes/v1/accounts.py
+++ server/routes/v1/accounts.py
@@ -1,5 +1,5 @@
 from apiflask import APIBlueprint
-from flask import jsonify
+from flask import jsonify, abort
 from adapters.aldus.accounts import AccountManager
 from adapters.aldus import projects
 from adapters.icns.resolver import get_icns_names
@@ -10,8 +10,12 @@ from utils.graphql.contracts import (
     get_graphql_contract_count_by_admin,
 )
 from utils.graphql.codes import get_graphql_codes_count_by_address
-from utils.graphql.proposals import get_graphql_proposals_count_by_address
+from utils.graphql.proposals import (
+    get_graphql_proposals_by_address,
+    get_graphql_proposals_count_by_address,
+)
 from utils.graphql.transactions import get_graphql_account_transactions_count
+
 
 accounts_bp = APIBlueprint("accounts", __name__)
 
@@ -70,3 +74,20 @@ def get_account_table_count(chain, network, account_address):
         "instantiated": instantiated_count,
         "contract_by_admin": contract_by_admin_count,
     }
+
+
+@accounts_bp.route(
+    "/<chain>/<network>/accounts/<account_address>/proposals", methods=["GET"]
+)
+def get_proposals(chain, network, account_address):
+    limit = get_query_param("limit", type=int, required=True)
+    offset = get_query_param("offset", type=int, required=True)
+    if limit < 0 or offset < 0:
+        abort(400, "Limit and offset must be non-negative")
+
+    data = get_graphql_proposals_by_address(
+        chain, network, limit, offset, account_address
+    )
+    for proposal in data.get("items", []):
+        proposal["proposer"] = account_address
+    return data
