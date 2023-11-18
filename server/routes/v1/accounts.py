84ad7f462dbfe2234a84c5626ecf2e4d67fd49c1
evilpeach <kanisorn.peach@gmail.com>
2023-11-18 15:00:46 +0700

    feat: account proposal

--- server/routes/v1/accounts.py
+++ server/routes/v1/accounts.py
@@ -1,8 +1,10 @@
 from apiflask import APIBlueprint
-from flask import jsonify
+from flask import jsonify, abort
 from adapters.aldus.accounts import AccountManager
 from adapters.aldus import projects
 from adapters.icns.resolver import get_icns_names
+from utils.helper import get_query_param
+from utils.graphql.proposals import get_graphql_proposals_by_address
 
 accounts_bp = APIBlueprint("accounts", __name__)
 
@@ -27,3 +29,20 @@ def get_account_info(chain, network, account_address):
         return jsonify(error=str(e)), 500
 
     return {"project_info": project_info, "public_info": public_info, "icns": icns}, 200
+
+
+@accounts_bp.route(
+    "/<chain>/<network>/accounts/<account_address>/proposals", methods=["GET"]
+)
+def get_proposals(chain, network, account_address):
+    limit = get_query_param("limit", type=int, required=True)
+    offset = get_query_param("offset", type=int, required=True)
+    if limit < 0 or offset < 0:
+        abort(400, "Limit and offset must be non-negative")
+
+    data = get_graphql_proposals_by_address(
+        chain, network, limit, offset, account_address
+    )
+    for proposal in data.get("items", []):
+        proposal["proposer"] = account_address
+    return data
--- server/tests/test_accounts.py
+++ server/tests/test_accounts.py
@@ -26,3 +26,28 @@ def test_get_account_info_failed():
         f"/v1/{chain}/{network}/accounts/{account_address}/info"
     )
     assert response.status_code == 500
+
+
+def test_get_proposals_by_address():
+    chain = "osmosis"
+    network = "osmo-test-5"
+    limit = 10
+    offset = 0
+    address = "osmo1acqpnvg2t4wmqfdv8hq47d3petfksjs5r9t45p"
+
+    response = app.test_client().get(
+        f"/v1/{chain}/{network}/accounts/{address}/proposals?limit={limit}&offset={offset}"
+    )
+    assert response.status_code == 200
+
+    response_json = response.json
+
+    for item in response_json["items"]:
+        assert type(item["deposit_end_time"]) == str
+        assert type(item["id"]) == int
+        assert type(item["is_expedited"]) == bool
+        assert type(item["resolved_height"]) == int
+        assert type(item["status"]) == str
+        assert type(item["title"]) == str
+        assert type(item["type"]) == str
+        assert type(item["voting_end_time"]) == str
