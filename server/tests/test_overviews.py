cf7395af9d519ab407276f7a21497301d2df9f78
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-11-30 11:20:18 +0700

    feat: add invalid network stats test

--- server/tests/test_overviews.py
+++ server/tests/test_overviews.py
@@ -1,7 +1,7 @@
 from app import app
 
 
-def test_get_account_info():
+def test_get_overviews_stats():
     chain = "osmosis"
     network = "osmosis-1"
 
@@ -13,3 +13,16 @@ def test_get_account_info():
     assert type(response_json["latest_block"]["height"]) == int
     assert type(response_json["latest_block"]["timestamp"]) == str
     assert type(response_json["transaction_count"]) == int
+
+
+def test_get_overviews_stats_invalid_network():
+    chain = "alles"
+    network = "celatone-1"
+
+    response = app.test_client().get(f"/v1/{chain}/{network}/overviews/stats")
+    assert response.status_code == 200
+
+    response_json = response.get_json()
+    assert response_json["block_time"] == None
+    assert response_json["latest_block"] == None
+    assert response_json["transaction_count"] == None
