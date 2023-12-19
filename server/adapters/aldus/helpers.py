51536ab2db478148f17c39cb93e2edd64e83f632
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-12-19 11:47:59 +0700

    fix: miniwasm params

--- server/adapters/aldus/helpers.py
+++ server/adapters/aldus/helpers.py
@@ -1,25 +1,29 @@
 import json
-import requests
 
+import requests
 from utils.gcs import get_network_data
 
 
 def get_upload_access(chain, network):
-    upload_access = {}
     res = requests.get(f"{get_network_data(chain,network,'lcd')}/wasm/params")
     if res.status_code == 200:
-        upload_access = res.json().get("params", {}).get("code_upload_access", {})
-    else:
-        res = requests.get(
-            f"{get_network_data(chain,network,'lcd')}/cosmos/params/v1beta1/params?subspace=wasm&key=uploadAccess"
-        ).json()
-        res_value = json.loads(res["param"]["value"])
-        permission = res_value["permission"]
-        addresses = res_value.get("addresses", [])
-        address = addresses[0] if permission == "OnlyAddress" else ""
-        upload_access = {
-            "permission": permission,
-            "addresses": addresses,
-            "address": address,
-        }
-    return upload_access
+        return res.json().get("params", {}).get("code_upload_access", {})
+
+    res = requests.get(
+        f"{get_network_data(chain,network,'lcd')}/cosmwasm/wasm/v1/codes/params"
+    )
+    if res.status_code == 200:
+        return res.json().get("params", {}).get("code_upload_access", {})
+
+    res = requests.get(
+        f"{get_network_data(chain,network,'lcd')}/cosmos/params/v1beta1/params?subspace=wasm&key=uploadAccess"
+    ).json()
+    res_value = json.loads(res["param"]["value"])
+    permission = res_value["permission"]
+    addresses = res_value.get("addresses", [])
+    address = addresses[0] if permission == "OnlyAddress" else ""
+    return {
+        "permission": permission,
+        "addresses": addresses,
+        "address": address,
+    }
